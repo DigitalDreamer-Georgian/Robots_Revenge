4610653c63a581391b817732e773a7804a3cd91c
Dragonfire17 <tylerung452@gmail.com>
2024-11-18 00:05:05 -0500

    tried adding ai
    added really wonky ai it follows players but doesnt acctually function

---
+++ Assets/Scripts/Enemies/Enemy movement.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class Enemymovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private float _rotationSpeed;
+
+    private Rigidbody2D _rigidbody;
+    private Playerawarnessss _playerAwarenessController;
+    private Vector2 _targetDirection;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _playerAwarenessController = GetComponent<Playerawarnessss>();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateTargetDirection();
+        RotateTowardsTarget();
+        SetVelocity();
+    }
+
+    private void UpdateTargetDirection()
+    {
+        if (_playerAwarenessController.AwareOfPlayer)
+        {
+            _targetDirection = _playerAwarenessController.DirectionToPlayer;
+        }
+        else
+        {
+            _targetDirection = Vector2.zero;
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        if (_targetDirection == Vector2.zero)
+        {
+            return;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetDirection);
+        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+        _rigidbody.SetRotation(rotation);
+    }
+
+    private void SetVelocity()
+    {
+        if (_targetDirection == Vector2.zero)
+        {
+            _rigidbody.velocity = Vector2.zero;
+        }
+        else
+        {
+            _rigidbody.velocity = transform.up * _speed;
+        }
+    }
+}

99add4d9069c55fb7f2e0c7c70bb6e2026805afa
Dragonfire17 <tylerung452@gmail.com>
2025-01-30 13:12:02 -0500

    added camera following and a load scene

---
+++ Assets/Scripts/losd scenes.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class losdscenes : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
